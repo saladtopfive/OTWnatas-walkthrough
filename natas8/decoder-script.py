0b4d6441044eadfdad8c19a113678b34ca48d8b4
saladtopfive <ignacychrobak@interia.pl>
2025-07-24 09:30:37 +0200

    natas8-solutionv5

--- natas8/decoder-script.py
+++ natas8/decoder-script.py
@@ -3,9 +3,7 @@ import base64
 print('enter the encoded secret:')
 secret = input()
 secret = bytes.fromhex(secret)
-print(secret)
 secret = secret[::-1]
 secret = base64.decodebytes(secret)
 
-print("decoded secret: ", secret ,"(b' YOUR DECODED SECRET ')")
-
+print("Decoded secret:", secret.decode('utf-8', errors='replace'))
